6af314b2ba0bbc781d5873b1c8acea01290840bc
Ara Periyan <ara.periyan-extern@invia.de>
2022-11-17 12:11:26 +0400

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends Statel essWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
